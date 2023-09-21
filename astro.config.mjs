c21d472f3933aa88bda09150ec1c8627236d72e5
James Heffernan <james@hefflabs.com>
2023-09-21 16:46:09 -0400

    Lets try again

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,10 @@
 import { defineConfig } from "astro/config";
-import cloudflare from "@astrojs/cloudflare";
 import tailwind from "@astrojs/tailwind";
 import svelte from "@astrojs/svelte";
 import prefetch from "@astrojs/prefetch";
 
 // https://astro.build/config
 export default defineConfig({
-  output: "server",
-  adapter: cloudflare({ mode: "directory" }),
+  output: "static",
   integrations: [tailwind(), svelte(), prefetch()],
 });
