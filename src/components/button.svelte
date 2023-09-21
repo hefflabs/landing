885eddd5c142f6420b42cd8fdce61fbff4a6c036
James Heffernan <james@hefflabs.com>
2023-09-21 16:55:03 -0400

    Lets try again

--- src/components/button.svelte
+++ src/components/button.svelte
@@ -1,7 +1,4 @@
-<button
-  on:click
-  class="flex flex-row gap-3 p-2 bg-glass rounded-lg font-medium hover:bg-glass-hover transition-colors disabled:opacity-40 disabled:cursor-not-allowed"
->
+<button on:click class="btn">
   <slot name="prefix" />
   <slot class="leading-6" />
   <slot name="suffix" />
